a8c16a2c00f9eef11e08548e9598d58ebceba6fa
aemino <aeminodev@gmail.com>
2021-03-24 17:44:04 -0700

    Clean up teams api

--- src/api/teams/mod.rs
+++ src/api/teams/mod.rs
@@ -1,32 +1,48 @@
-use crate::models::Team;
-use actix_web::{web, HttpResponse};
-use butane::{db::Connection, query, DataObject};
-use rand::prelude::*;
 use std::ops::RangeInclusive;
 
-const TOKEN_LEN: usize = 32;
-const TOKEN_CHAR_RANGE: RangeInclusive<u8> = 0x21..=0x7e;
+use actix_web::{HttpResponse, Responder, web};
+use butane::{db::Connection, query, DataObject};
+use rand::prelude::*;
+
+use crate::models::Team;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::resource("/")
-            .route(web::post().to(create_team))
             .route(web::get().to(get_team))
+            .route(web::post().to(create_team))
     );
 }
 
-async fn create_team(name: web::Json<String>, db: web::Data<Connection>) -> HttpResponse {
+fn generate_token<R: Rng + CryptoRng + ?Sized>(rng: &mut R) -> String {
+    const TOKEN_LEN: usize = 32;
+    const TOKEN_CHAR_RANGE: RangeInclusive<u8> = 0x21..=0x7e;
+
+    (0..TOKEN_LEN)
+        .map(|_| char::from(rng.gen_range(TOKEN_CHAR_RANGE)))
+        .collect()
+}
+
+async fn get_team(web::Path(id): web::Path<i64>, db: web::Data<Connection>) -> impl Responder {
+    let team = Team::get(db.as_ref(), id).unwrap();
+
+    let resp_team: backend_models::Team = team.into();
+    HttpResponse::Ok().json(&resp_team)
+}
+
+async fn create_team(name: web::Json<String>, db: web::Data<Connection>) -> impl Responder {
     let name = name.clone();
 
-    let test = query!(Team, name == { name }).load_first(db.as_ref()).unwrap();
-    if test.is_some() {
+    let existing_team = query!(Team, name == { name })
+        .load_first(db.as_ref()).unwrap();
+
+    if existing_team.is_some() {
         return HttpResponse::Conflict().finish();
     }
 
     let mut rng = thread_rng();
-    let token: String = (0..TOKEN_LEN)
-        .map(|_| -> char { rng.gen_range(TOKEN_CHAR_RANGE).into() })
-        .collect();
+    let token = generate_token(&mut rng);
+
     let mut new_team = Team {
         token,
         name,
@@ -36,14 +52,5 @@ async fn create_team(name: web::Json<String>, db: web::Data<Connection>) -> Http
     new_team.save(db.as_ref()).unwrap();
 
     let resp_team: backend_models::TeamPrivateDetails = new_team.into();
-    HttpResponse::Ok().body(serde_json::to_string(&resp_team).unwrap())
-}
-
-async fn get_team(
-    web::Path(id): web::Path<i64>,
-    db: web::Data<Connection>,
-) -> HttpResponse {
-    let team = Team::get(db.as_ref(), id).unwrap();
-    let resp_team: backend_models::Team = team.into();
-    HttpResponse::Ok().body(serde_json::to_string(&resp_team).unwrap())
+    HttpResponse::Ok().json(&resp_team)
 }
